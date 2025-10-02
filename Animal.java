76b80e3e6342b1d39c8ddf8885929b69dfa1550b
mehraUTD <mehraUTD@users.noreply.github.com>
2025-10-02 15:54:27 -0500

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,68 +1,3 @@
-/*
-Classes & Objects
-
-1 - Defining classes and objects  
-2 - Instance variables and methods  
-3 - Constructors (default, overloaded) 
-4 - Constructor chaining (this())  
-5 - The this keyword  
-6 - Accessing objects and fields (. operator or access modifier)  
-7 - UML basics (class diagrams)  
-8 - Class abstraction and encapsulation  
-9 - Static variables and methods
-10 - Constants (static final)  
-   - Visibility modifiers  (public (+) - protected (#) - package(~) - private(-)) )
-11 - Getter and setter methods  
-12 - Arrays of objects  
-13 - Immutable objects 
-14 - The toString() method 
-15 - The equals() method 
-
----
-
-Inheritance
-
-16. Superclass and Subclass
-17. The `extends` Keyword
-18. Inheritance of Fields and Methods
-19. Constructor Chaining (`super()`)
-20. The `super` Keyword (super. in method)
-21. Method Overriding
-22. Abstract Methods (stopped here section11)
-23. Object Casting (Upcasting, Downcasting)
-
-Polymorphism
-
-24. Polymorphism (Dynamic Binding)
-25. The `instanceof` Operator (more example..)
-
-Interfaces & Abstract Classes
-
-26. Interfaces (`interface`, `implements`)
-27. Abstract Classes in Depth
-28. Multiple Inheritance of Type (Interfaces)
-29. Implementing Multiple Interfaces
-30. The `Comparable<T>` Interface (`compareTo`)
-31. The `Cloneable` Interface (`clone`)
-Note: make sure to also show the array of comparable objects even if they are not 
-the same type of objects, like edible. 
-
-Generics
-
-32. Motivation for Generics (Type Safety, Eliminating Casting)
-33. Generic Classes (`GenericClass<T>`)
-34. Bounded Type Parameters (`<T extends Number>`)
-35. Multiple Bounds (`<T extends A & B>`)
-36. Generic Methods (`<T> T methodName(...)`)
-37. Raw Types (and Why to Avoid Them)
-38. Wildcards – Unbounded (`<?>`)
-39. Wildcards – Upper Bounded (`<? extends T>`)
-40. Wildcards – Lower Bounded (`<? super T>`)
-41. Generic Collections (`ArrayList<T>`, `HashMap<K,V>`)
-42. Restrictions and Limitations of Generics
-
- */
-
 
 public abstract class Animal implements Comparable<Animal>, Cloneable{
 
@@ -251,6 +186,20 @@ interface Jumper{
 
 interface Runner{
 	void run();
+}
+
+class Athlete<T extends Runner & Jumper>{
+	private T participant;
+	 
+	Athlete(T participant){
+		this.participant = participant;
+	}
+	
+	public void perform() {
+		participant.run();
+		participant.jump();
+	}
+	
 }
 
 
